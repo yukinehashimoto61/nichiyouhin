daa9629f834545353835ea9a45542e100a73416f
yukinehashimoto61 <yukine.hashimoto.0601@gmail.com>
2026-08-11 16:57:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nichiyohin-v3";
+const CACHE = "nichiyohin-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
